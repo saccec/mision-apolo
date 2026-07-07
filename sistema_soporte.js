6cb9b6866c2f3c36bdba169ba900c81d700f2ff9
Luis Salvador <saccec@gmail.com>
2026-07-06 21:34:52 -0500

    Feat: Incorporaci[on el lazo for

--- sistema_soporte.js
+++ sistema_soporte.js
@@ -1,9 +1,11 @@
 //Ejemplo de cambio en archivo
-function prototipo(){
+function prototipo() {
     //Agregamos otro comentario
-    var i=0;
-     console.log("Oxigeno al 10%");
-     console.log("Otro cambio");
-     return i;
+    var i = 0;
+    for (i = 0; i < 100; i++) {
+        console.log("Oxigeno al 10%");
+        console.log("Otro cambio");
+        return i;
+    }
 }
 prototipo();
